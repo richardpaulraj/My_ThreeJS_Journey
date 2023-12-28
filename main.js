5c1785345bb7320c958ca4751862d2a5f5b79834
Richard Paul <richardpaulraj21@gmail.com>
2023-12-28 14:14:55 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>100DaysOfCode with Three-Js</h1>
+
+        <a href ='https://fist-box.vercel.app/' target="_blank"><button>Day-1</button></a>
+        <a href ='https://first-buffer-geometry.vercel.app/' target="_blank"><button>Day-2</button></a>
+        <a href ='https://materials-richard-pauls-projects.vercel.app/' target="_blank"><button>Day-3</button></a>
+        <a href ='https://first-3d-text-richard-pauls-projects.vercel.app/' target="_blank"><button>Day-4</button></a>
+        <a href ='https://haunted-house-richard-pauls-projects.vercel.app/' target="_blank"><button>Day-5</button></a>
+        <a href ='https://twitter.com/richardpaulraj/status/1739588629377474735' target="_blank"><button>Day-6</button></a>
+        <a href ='https://galaxy-generator-richard-pauls-projects.vercel.app/' target="_blank"><button>Day-7</button></a>
+        <a href ='#'><button>Day-8</button></a>
+        <a href ='#'><button>Day-9</button></a>
+        <a href ='#'><button>Day-10</button></a>
+  </div>
+`
+
+
